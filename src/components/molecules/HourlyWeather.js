71ba601e03a128a4fdb8e80f19e38fd07f7badb9
“jojo-baker” <jo.alice.baker@gmail.com>
2019-11-10 15:28:33 +0800

    Fixed timezone

--- src/components/molecules/HourlyWeather.js
+++ src/components/molecules/HourlyWeather.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import HourlyWeatherItem from './HourlyWeatherItem';
 import moment from 'moment';
+import 'moment-timezone';
 import './HourlyWeather.css';
 
-const HourlyWeather = ({ list, ...props }) => {
-    return (
+const HourlyWeather = ({ list, ...props }) => { 
+
+  return (
       <div className="HourlyWeather">
         <div className="HWList">
           <ul>
             {list.map(item => (
               <HourlyWeatherItem
                 key={item.dt}
-                time={moment(item.dt_txt).format('ha')}
+                // time={moment(item.dt_txt).format('ha')}
+                time={moment.unix(item.dt).format('ha')}
                 icon={item.weather[0].icon}
                 maxTemp={`${parseInt(item.main.temp_max)}`}
               />
             ))}
-          </ul>
+        
+         </ul>
           </div>
       </div>
     );
+  
   };
 
 export default HourlyWeather;
